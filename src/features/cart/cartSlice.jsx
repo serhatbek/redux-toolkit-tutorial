407937c9655d2e5678cc432c75b4336e0cf30967
serhatbek <serhat.bekk@gmail.com>
2023-10-11 14:25:58 +0300

    Reducers - Remove Item

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems';
 
 const initialState = {
   cartItems: cartItems,
-  amount: 2,
+  amount: 1,
   total: 0,
   isLoading: true,
 };
@@ -15,11 +15,27 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeCartItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
     resetCart: (state) => {
       state.cartItems = cartItems;
     },
+    // increaseCart: (state) => {
+    //   state.amount = amount + 1;
+    // },
+    // decreaseCart: (state) => {
+    //   state.amount = amount > 1 ? amount - 1 : 0;
+    // },
   },
 });
 
 export default cartSlice.reducer;
-export const { clearCart, resetCart } = cartSlice.actions;
+export const {
+  clearCart,
+  removeCartItem,
+  resetCart,
+  increaseCart,
+  decreaseCart,
+} = cartSlice.actions;
