8d6156746c23619a955653529bd69bde95be811a
serhatbek <serhat.bekk@gmail.com>
2023-10-12 15:34:14 +0300

    createAsyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import CartContainer from './components/CartContainer/CartContainer';
 import Navbar from './components/Navbar/Navbar';
 import { useEffect } from 'react';
-import { calculateTotal } from './features/cart/cartSlice';
+import { calculateTotal, getCartItems } from './features/cart/cartSlice';
 import Modal from './components/Modal/Modal';
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
 
   useEffect(() => {
     dispatch(calculateTotal());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,12 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
 
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
 const initialState = {
-  cartItems: cartItems,
+  // cartItems: cartItems,
+  cartItems: [],
   cartAmount: 0,
   total: 0,
   isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', async () => {
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+});
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -36,6 +45,20 @@ const cartSlice = createSlice({
       });
       state.cartAmount = amount;
       state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      // console.log(action);
+      // console.log(action.payload);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
     },
   },
 });
